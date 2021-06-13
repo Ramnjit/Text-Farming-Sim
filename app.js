fd8d11013487b786d313100118ac77d3d82bda02
Ramnjit S. Boparai <ramnjit@hotmail.com>
2021-06-12 23:47:08 -0500

    app.css check

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 console.log("Text Farming Sim");
+
+console.log("hi!");
+
+//action 
+//inventory
+//store
+//sleep
+//quit
+
+//Day
+//Energy
+
+// let enegry = 100;
+// let day = 0;
+
+// while(enegry > 0){
+
+
+
+
+// }
+
+
+// function endDay(){
+//     day++;
+
+// }
+
+// function action(action){
+//     if(action=='water'){
+
+
+
+//     }
+
+//     if(action == 'harvest')
+// }
